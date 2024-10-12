03b001e21f06757dff9bbb3b93f3142b367b98c3
DevVizlo <dev.vizlo.com@gmail.com>
2024-10-12 12:41:07 +0300

    add selenium code commit

---
+++ tests/test_2_open_site.py
@@ -0,0 +1,13 @@
+﻿from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+textFormUsername = driver.find_element(By.CSS_SELECTOR, '#user-name')
+textFormPassword = driver.find_element(By.CSS_SELECTOR, '#password')
+textFromLoginButton = driver.find_element(By.CSS_SELECTOR, '#login-button')
+
+if textFormUsername is None and textFormPassword is None and textFromLoginButton is None:
+    print('Не все элементы обнаружены')
+else:
+    print('Все элементы обнаружены')
