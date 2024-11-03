71907e7c01b9b5176d4fae53301a61e06b8ac1e4
Vizlo <109802629+DevVizlo@users.noreply.github.com>
2024-11-03 12:12:38 +0300

    Update test_2_open_site.py

--- tests/test_2_open_site.py
+++ tests/test_2_open_site.py
@@ -1,13 +1,17 @@
-﻿from selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
 
-textFormUsername = driver.find_element(By.CSS_SELECTOR, '#user-name')
-textFormPassword = driver.find_element(By.CSS_SELECTOR, '#password')
-textFromLoginButton = driver.find_element(By.CSS_SELECTOR, '#login-button')
+def checkLoginPage():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
 
-if textFormUsername is None and textFormPassword is None and textFromLoginButton is None:
-    print('Не все элементы обнаружены')
-else:
-    print('Все элементы обнаружены')
+    textFormUsername = driver.find_element(By.CSS_SELECTOR, '#user-name')
+    textFormPassword = driver.find_element(By.CSS_SELECTOR, '#password')
+    textFromLoginButton = driver.find_element(By.CSS_SELECTOR, '#login-button')
+
+    if textFormUsername is None and textFormPassword is None and textFromLoginButton is None:
+        print('Не все элементы обнаружены')
+    else:
+        print('Все элементы обнаружены')
+
+checkLoginPage()
